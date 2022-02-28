24d3a64bd78193d826fd1d4e79ced06f71be8b1d
Levon Kirakosyan <noosx.e@gmail.com>
2022-02-27 16:22:55 -0800

    Configure peer dep handling and type output.

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,39 @@
 import typescript from "rollup-plugin-typescript2";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import dts from "rollup-plugin-dts";
 
 import pkg from "./package.json";
 
 /**
  * @type {import('rollup').RollupOptions}
  */
-const config = {
-  input: "./src/index.tsx",
-  output: [
-    {
-      file: `dist/${pkg.name}.js`,
-      format: "umd",
-      name: pkg.name,
-      esModule: false,
-      exports: "named",
-      sourcemap: true,
-    },
-    {
-      dir: "dist/esm",
-      format: "esm",
-      exports: "named",
-      sourcemap: true,
-    },
-    {
-      dir: "dist/cjs",
-      format: "cjs",
-      exports: "named",
-      sourcemap: true,
-    },
-  ],
-  plugins: [typescript()],
-};
+const config = [
+  {
+    input: "./src/index.tsx",
+    output: [
+      {
+        file: pkg.module,
+        format: "esm",
+        exports: "named",
+        sourcemap: true,
+      },
+      {
+        file: pkg.main,
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
+      },
+    ],
+    plugins: [peerDepsExternal(), nodeResolve(), commonjs(), typescript()],
+    external: ["react"],
+  },
+  {
+    input: "dist/esm/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
 
 export default config;
